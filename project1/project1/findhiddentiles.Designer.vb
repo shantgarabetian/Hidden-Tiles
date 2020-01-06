57e0d286f830e9ed6f26381624ebd3c42a4ebd57
SHANT GARABETIAN <39273575+shantgarabetian@users.noreply.github.com>
2020-01-06 14:16:20 -0800

    Add files via upload

---
+++ project1/project1/findhiddentiles.Designer.vb
@@ -0,0 +1,277 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class findhiddentiles
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.random = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.score = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'random
+        '
+        Me.random.BackColor = System.Drawing.Color.Lime
+        Me.random.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.random.ForeColor = System.Drawing.Color.White
+        Me.random.Location = New System.Drawing.Point(558, 33)
+        Me.random.Name = "random"
+        Me.random.Size = New System.Drawing.Size(159, 51)
+        Me.random.TabIndex = 0
+        Me.random.Text = "START "
+        Me.random.UseVisualStyleBackColor = False
+        '
+        'Timer1
+        '
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(581, 127)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 20)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "timer"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(102, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(260, 34)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Find Hidden tiles"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(615, 265)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 23)
+        Me.Label3.TabIndex = 12
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Black
+        Me.Button1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.Black
+        Me.Button1.Location = New System.Drawing.Point(45, 59)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 103)
+        Me.Button1.TabIndex = 13
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Black
+        Me.Button2.Font = New System.Drawing.Font("Arial Rounded MT Bold", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.Black
+        Me.Button2.Location = New System.Drawing.Point(181, 59)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 103)
+        Me.Button2.TabIndex = 14
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Black
+        Me.Button3.Font = New System.Drawing.Font("Arial Rounded MT Bold", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.Color.Black
+        Me.Button3.Location = New System.Drawing.Point(307, 59)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 103)
+        Me.Button3.TabIndex = 15
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.Black
+        Me.Button4.Font = New System.Drawing.Font("Arial Rounded MT Bold", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.ForeColor = System.Drawing.Color.Black
+        Me.Button4.Location = New System.Drawing.Point(46, 168)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(100, 103)
+        Me.Button4.TabIndex = 16
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.Black
+        Me.Button5.Font = New System.Drawing.Font("Arial Rounded MT Bold", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.ForeColor = System.Drawing.Color.Black
+        Me.Button5.Location = New System.Drawing.Point(181, 168)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(100, 103)
+        Me.Button5.TabIndex = 17
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.Black
+        Me.Button6.Font = New System.Drawing.Font("Arial Rounded MT Bold", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.ForeColor = System.Drawing.Color.Black
+        Me.Button6.Location = New System.Drawing.Point(307, 168)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(100, 103)
+        Me.Button6.TabIndex = 18
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.Black
+        Me.Button7.Font = New System.Drawing.Font("Arial Rounded MT Bold", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.ForeColor = System.Drawing.Color.Black
+        Me.Button7.Location = New System.Drawing.Point(46, 277)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(100, 103)
+        Me.Button7.TabIndex = 19
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.Black
+        Me.Button8.Font = New System.Drawing.Font("Arial Rounded MT Bold", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.ForeColor = System.Drawing.Color.Black
+        Me.Button8.Location = New System.Drawing.Point(181, 277)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(100, 103)
+        Me.Button8.TabIndex = 20
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.Color.Black
+        Me.Button9.Font = New System.Drawing.Font("Arial Rounded MT Bold", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button9.ForeColor = System.Drawing.Color.Black
+        Me.Button9.Location = New System.Drawing.Point(307, 273)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(100, 103)
+        Me.Button9.TabIndex = 21
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Arial Rounded MT Bold", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Maroon
+        Me.Label4.Location = New System.Drawing.Point(487, 324)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 20)
+        Me.Label4.TabIndex = 22
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Arial Rounded MT Bold", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.DarkGreen
+        Me.Label5.Location = New System.Drawing.Point(661, 324)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(0, 20)
+        Me.Label5.TabIndex = 23
+        '
+        'score
+        '
+        Me.score.AutoSize = True
+        Me.score.Font = New System.Drawing.Font("Arial Rounded MT Bold", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.score.ForeColor = System.Drawing.Color.Blue
+        Me.score.Location = New System.Drawing.Point(526, 405)
+        Me.score.Name = "score"
+        Me.score.Size = New System.Drawing.Size(0, 28)
+        Me.score.TabIndex = 24
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(545, 226)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(192, 23)
+        Me.Label6.TabIndex = 25
+        Me.Label6.Text = "TOTAL ATTEMPTS"
+        '
+        'findhiddentiles
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(868, 450)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.score)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.random)
+        Me.Name = "findhiddentiles"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents random As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents score As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ project1/project1/findhiddentiles.vb
@@ -0,0 +1,365 @@
+﻿Public Class findhiddentiles
+    Dim pick1 As Integer
+    Dim pick2 As Integer
+    Dim pick3 As Integer
+    Dim clicks As Integer = 0
+    Dim timerValue As Integer = 3000
+    Dim totalRight As Integer = 0
+    Dim totalWrong As Integer = 0
+    Dim winningScore As Integer = 0
+
+
+    ' STARTING THE GAME AND TIMER
+    ' DISABLE BUTTONS AS REQUIRMENT
+    Private Sub random_Click(sender As Object, e As EventArgs) Handles random.Click
+        clear()
+
+        Button1.Text = ""
+        Button2.Text = ""
+        Button3.Text = ""
+        Button4.Text = ""
+        Button5.Text = ""
+        Button6.Text = ""
+        Button7.Text = ""
+        Button8.Text = ""
+        Button9.Text = ""
+
+        Label3.Text = " "
+        Label4.Text = " "
+        Label5.Text = " "
+        random.Enabled = False
+
+        Timer1.Interval = timerValue
+        Label1.Text = "TIMER IS ON "
+        Timer1.Start()
+
+        Button1.Enabled = False
+        Button2.Enabled = False
+        Button3.Enabled = False
+        Button4.Enabled = False
+        Button5.Enabled = False
+        Button6.Enabled = False
+        Button7.Enabled = False
+        Button8.Enabled = False
+        Button9.Enabled = False
+
+        ' CREATING 3 RANDOM RED COLORS IN 9 TILES
+        Dim random_number As Random = New Random
+        pick1 = random_number.Next(1, 10)
+        Do
+            pick2 = random_number.Next(1, 10)
+
+        Loop While pick1 = pick2
+        Do
+            pick3 = random_number.Next(1, 10)
+
+        Loop While pick3 = pick1 Or pick3 = pick2
+
+        ' FIRST RANDOM RED
+        If pick1 = 1 Then
+            Button1.BackColor = Color.Red
+        End If
+        If pick1 = 2 Then
+            Button2.BackColor = Color.Red
+        End If
+        If pick1 = 3 Then
+            Button3.BackColor = Color.Red
+        End If
+        If pick1 = 4 Then
+            Button4.BackColor = Color.Red
+        End If
+        If pick1 = 5 Then
+            Button5.BackColor = Color.Red
+        End If
+        If pick1 = 6 Then
+            Button6.BackColor = Color.Red
+        End If
+        If pick1 = 7 Then
+            Button7.BackColor = Color.Red
+        End If
+        If pick1 = 8 Then
+            Button8.BackColor = Color.Red
+        End If
+        If pick1 = 9 Then
+            Button9.BackColor = Color.Red
+        End If
+
+        ' SECOND RANDOM RED 
+        If pick2 = 1 Then
+            Button1.BackColor = Color.Red
+        End If
+        If pick2 = 2 Then
+            Button2.BackColor = Color.Red
+        End If
+        If pick2 = 3 Then
+            Button3.BackColor = Color.Red
+        End If
+        If pick2 = 4 Then
+            Button4.BackColor = Color.Red
+        End If
+        If pick2 = 5 Then
+            Button5.BackColor = Color.Red
+        End If
+        If pick2 = 6 Then
+            Button6.BackColor = Color.Red
+        End If
+        If pick2 = 7 Then
+            Button7.BackColor = Color.Red
+        End If
+        If pick2 = 8 Then
+            Button8.BackColor = Color.Red
+        End If
+        If pick2 = 9 Then
+            Button9.BackColor = Color.Red
+        End If
+
+        ' THIRD RANDOM RED
+        If pick3 = 1 Then
+            Button1.BackColor = Color.Red
+        End If
+        If pick3 = 2 Then
+            Button2.BackColor = Color.Red
+        End If
+        If pick3 = 3 Then
+            Button3.BackColor = Color.Red
+        End If
+        If pick3 = 4 Then
+            Button4.BackColor = Color.Red
+        End If
+        If pick3 = 5 Then
+            Button5.BackColor = Color.Red
+        End If
+        If pick3 = 6 Then
+            Button6.BackColor = Color.Red
+        End If
+        If pick3 = 7 Then
+            Button7.BackColor = Color.Red
+        End If
+        If pick3 = 8 Then
+            Button8.BackColor = Color.Red
+        End If
+        If pick3 = 9 Then
+            Button9.BackColor = Color.Red
+        End If
+    End Sub
+
+    ' TIMER IS OFF AND TILES ARE COVERD IN BLACK AND ENABLED
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label1.Text = "TIME IS DONE"
+        Timer1.Stop()
+        Button1.BackColor = Color.Black
+        Button2.BackColor = Color.Black
+        Button3.BackColor = Color.Black
+        Button4.BackColor = Color.Black
+        Button5.BackColor = Color.Black
+        Button6.BackColor = Color.Black
+        Button7.BackColor = Color.Black
+        Button8.BackColor = Color.Black
+        Button9.BackColor = Color.Black
+
+        Button1.Enabled = True
+        Button2.Enabled = True
+        Button3.Enabled = True
+        Button4.Enabled = True
+        Button5.Enabled = True
+        Button6.Enabled = True
+        Button7.Enabled = True
+        Button8.Enabled = True
+        Button9.Enabled = True
+
+    End Sub
+    ' CLEAR FUNCTION TO REST COLORS
+    Private Sub clear()
+        Button1.BackColor = Color.Black
+        Button2.BackColor = Color.Black
+        Button3.BackColor = Color.Black
+        Button4.BackColor = Color.Black
+        Button5.BackColor = Color.Black
+        Button6.BackColor = Color.Black
+        Button7.BackColor = Color.Black
+        Button8.BackColor = Color.Black
+        Button9.BackColor = Color.Black
+
+    End Sub
+    ' CHECKING BUTTONS FOR RED SELECTION AND SETTING [ O OR X ] AND COUNTERS FOR RESULTS
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If pick1 = 1 Or pick2 = 1 Or pick3 = 1 Then
+            Button1.BackColor = Color.Red
+            Button1.Text = " O "
+            Button1.Enabled = False
+            totalRight += 1
+        Else
+            Button1.BackColor = Color.Blue
+            Button1.Text = " X "
+            totalWrong += 1
+            Button1.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If pick1 = 2 Or pick2 = 2 Or pick3 = 2 Then
+            Button2.BackColor = Color.Red
+            Button2.Text = " O "
+            Button2.Enabled = False
+            totalRight += 1
+
+        Else
+            Button2.BackColor = Color.Blue
+            Button2.Text = " X "
+            totalWrong += 1
+            Button2.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If pick1 = 3 Or pick2 = 3 Or pick3 = 3 Then
+            Button3.BackColor = Color.Red
+            Button3.Text = " O "
+            Button3.Enabled = False
+            totalRight += 1
+
+        Else
+            Button3.BackColor = Color.Blue
+            Button3.Text = " X "
+            totalWrong += 1
+            Button3.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If pick1 = 4 Or pick2 = 4 Or pick3 = 4 Then
+            Button4.BackColor = Color.Red
+            Button4.Text = " O "
+            Button4.Enabled = False
+            totalRight += 1
+
+        Else
+            Button4.BackColor = Color.Blue
+            Button4.Text = " X "
+            totalWrong += 1
+            Button4.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If pick1 = 5 Or pick2 = 5 Or pick3 = 5 Then
+            Button5.BackColor = Color.Red
+            Button5.Text = " O "
+            Button5.Enabled = False
+            totalRight += 1
+
+        Else
+            Button5.BackColor = Color.Blue
+            Button5.Text = " X "
+            totalWrong += 1
+            Button5.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If pick1 = 6 Or pick2 = 6 Or pick3 = 6 Then
+            Button6.BackColor = Color.Red
+            Button6.Text = " O "
+            Button6.Enabled = False
+            totalRight += 1
+
+        Else
+            Button6.BackColor = Color.Blue
+            Button6.Text = " X "
+            totalWrong += 1
+            Button6.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If pick1 = 7 Or pick2 = 7 Or pick3 = 7 Then
+            Button7.BackColor = Color.Red
+            Button7.Text = " O "
+            Button7.Enabled = False
+            totalRight += 1
+
+        Else
+            Button7.BackColor = Color.Blue
+            Button7.Text = " X "
+            totalWrong += 1
+            Button7.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If pick1 = 8 Or pick2 = 8 Or pick3 = 8 Then
+            Button8.BackColor = Color.Red
+            Button8.Text = " O "
+            Button8.Enabled = False
+            totalRight += 1
+
+        Else
+            Button8.BackColor = Color.Blue
+            Button8.Text = " X "
+            totalWrong += 1
+            Button8.Enabled = False
+        End If
+    End Sub
+
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+
+        If pick1 = 9 Or pick2 = 9 Or pick3 = 9 Then
+            Button9.BackColor = Color.Red
+            Button9.Text = " O "
+            Button9.Enabled = False
+            totalRight += 1
+
+        Else
+            Button9.BackColor = Color.Blue
+            Button9.Text = " X "
+            totalWrong += 1
+            Button9.Enabled = False
+        End If
+    End Sub
+    ' BUTTONS CLICK SENDERS
+    Private Sub clicking(sender As Object, e As EventArgs) Handles Button1.Click, Button2.Click, Button3.Click, Button4.Click, Button5.Click, Button6.Click, Button7.Click, Button8.Click, Button9.Click
+
+        Dim btn As Button = sender
+        clickstotal(btn.Tag)
+
+    End Sub
+
+    ' TOTALS AND TIMER REST 
+    Private Sub clickstotal(coin As Decimal)
+
+        Label4.Text = " worng answer " & totalWrong
+
+        Label5.Text = " right answer " & totalRight
+
+        clicks = clicks + 1
+        Label3.Text = " " & clicks
+
+        If totalRight = 3 Then
+            winningScore += 1
+            score.Text = " Your Score is = " & winningScore
+
+        End If
+        If clicks = 3 Then
+            If totalRight = 3 And timerValue > 1000 Then
+                timerValue -= 1000
+            End If
+            Button1.Enabled = False
+            Button2.Enabled = False
+            Button3.Enabled = False
+            Button4.Enabled = False
+            Button5.Enabled = False
+            Button6.Enabled = False
+            Button7.Enabled = False
+            Button8.Enabled = False
+            Button9.Enabled = False
+            clicks = 0
+            totalWrong = 0
+            totalRight = 0
+            random.Enabled = True
+        End If
+
+    End Sub
+
+End Class
